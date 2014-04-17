be9a9e98f6e0180505576482d0c00ea4db2b5d4b
Jedrzej Danko <jedrzej.danko@mycharity.ie>
2014-04-17 15:13:10 +0200

    Calculations moved from Parser into Operation class

---
+++ classes/operation.rb
@@ -0,0 +1,84 @@
+class Operation < Array
+  
+  @@precedence = [['*', '/'], ['+', '-']]
+  @@operators = ['+', '-', '/', '*']
+  
+  def calculate
+    operation = self
+    @@precedence.each do |operators|
+      begin
+        operation = operation.reduce(operators)
+      end while operation.hasOperator?(operators)      
+    end
+    return operation         
+  end
+  
+  def readResult
+    numberOfImportantFields = 0
+    self.each do |val|
+      if val != nil 
+        numberOfImportantFields += 1
+      end
+    end
+    if numberOfImportantFields == 1
+      return self[0]
+    end
+    raise "Calculation is not finished yet: more then one value in operation"
+  end
+  
+  def hasOperator?(operators)
+    if operators.respond_to?('each')
+      operators.each do |operator|
+         
+        if hasOperator?(operator)
+          return true
+        end
+      end 
+      return false
+    end
+    return self.include?(operators)
+  end
+  
+  def reduce(operators)
+    self.each_index {|i|
+      if operators.include?(self[i])
+        self[i-1] = executeOperator(self[i], self[i-1], self[i+1]);
+        self.delete_at(i+1)
+        self.delete_at(i)
+        
+        return Operation.new(self)
+      end              
+    }
+    return Operation.new(self)
+  end
+  
+  private
+  
+  def executeOperator(operator, firstOperand, lastOperand)
+    return case
+      when operator.eql?('*') then multiply(firstOperand, lastOperand)
+      when operator.eql?('/') then divide(firstOperand, lastOperand)
+      when operator.eql?('-') then sub(firstOperand, lastOperand)
+      when operator.eql?('+') then add(firstOperand, lastOperand)
+    end
+  end
+  
+  
+  
+  def multiply(n1, n2)
+    return n1.to_f * n2.to_f
+  end
+  
+  def divide(n1, n2)
+    return n1.to_f / n2.to_f
+  end
+  
+  def add(n1, n2)
+    return n1.to_f + n2.to_f
+  end
+  
+  def sub(n1, n2)
+    return n1.to_f - n2.to_f
+  end
+  
+end 
--- classes/parser.rb
+++ classes/parser.rb
@@ -1,14 +1,14 @@
+require 'operation'
+
 class Parser
-  
-  @@operators = ['+', '-', '/', '*'] 
-  
+    
   def initialize(inputReader)
     if(inputReader.respond_to?('getInput') == false)
       raise 'getInput method is required'
     end
-    @command = inputReader.getInput
-    puts("Zadanie #{@command}")
-    @@result = calculate
+    operationString = inputReader.getInput
+    operation = Operation.new(parse(operationString)) 
+    @@result = operation.calculate.readResult
   end
   
   def getResult
@@ -17,82 +17,18 @@ class Parser
   
   private
   
-  def parse
+  def parse(operationString)
     pattern = /(?<number>[+]?(\d*[.])?\d+)(?<operators>[-*+\/]{1})?/
-    result = @command.scan(pattern)
+    result = operationString.scan(pattern)
     returns = Array.new
-    result.each {|match|
-      match.each {|str|
+    result.each do |match|
+      match.each do |str|
         returns.push(str)
-        } 
-    }
+      end
+    end
     return returns
   end
   
-  def calculate
-    parts = parse
-    reduced = Array.new
-    # detect multiplying and dividing
-    begin
-      elements = reduce(parts, ['*', '/'])      
-    end while hasOperator?(elements, ['*', '/'])
-    begin
-      elements = reduce(parts, ['+', '-'])
-    end while hasOperator?(elements, ['+', '-'])
-    
-    return elements[0]
-    
-  end
   
-  def reduce(elementsArray, operators)
-    elementsArray.each_index {|i|
-      if operators.include?(elementsArray[i])
-        elementsArray[i-1] = executeOperator(elementsArray[i], elementsArray[i-1], elementsArray[i+1]);
-        elementsArray.delete_at(i+1)
-        elementsArray.delete_at(i)
-        
-        return elementsArray
-      end              
-    }
-    return elementsArray
-  end
   
-  def hasOperator?(elements, operators)
-    if operators.respond_to?('each')
-      operators.each do |operator|
-         
-        if hasOperator?(elements, operator)
-          return true
-        end
-      end 
-      return false
-    end
-    return elements.include?(operators)
-  end
-  
-  def executeOperator(operator, firstOperand, lastOperand)
-    return case
-    when operator.eql?('*') then multiply(firstOperand, lastOperand)
-    when operator.eql?('/') then divide(firstOperand, lastOperand)
-    when operator.eql?('-') then sub(firstOperand, lastOperand)
-    when operator.eql?('+') then add(firstOperand, lastOperand)
-    end
-  end
-  
-  def multiply(n1, n2)
-    return n1.to_f * n2.to_f
-  end
-  
-  def divide(n1, n2)
-    return n1.to_f / n2.to_f
-  end
-  
-  def add(n1, n2)
-    return n1.to_f + n2.to_f
-  end
-  
-  def sub(n1, n2)
-    return n1.to_f - n2.to_f
-  end
-
 end
